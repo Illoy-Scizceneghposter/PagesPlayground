518c48a70b97e7ad4086e3a57c0022cdbfbf544b
Illoy-Scizceneghposter <illoyscizceneghposter@gmail.com>
2025-01-09 23:02:08 -0800

    Update cookieTest.js

--- cookieTest.js
+++ cookieTest.js
@@ -1,2 +1,3 @@
 document.cookie = "test1=lol";
 document.cookie = "test2=lol2; Domain=illoy-scizceneghposter.github.io";
+document.cookie = "session_id=abc123; path=/; domain=illoy-scizceneghposter.github.io; secure; samesite=None";
