4e88f2c027da6d1ac9d40f64839be719b46e0de1
Illoy-Scizceneghposter <illoyscizceneghposter@gmail.com>
2025-01-09 22:24:03 -0800

    Update cookieTest.js

--- cookieTest.js
+++ cookieTest.js
@@ -1,5 +1,5 @@
 document.cookie = "test1=lol";
-document.cookie = "test2=lol2";
+document.cookie = "test2=lol2; domain=illoy-scizceneghposter.github.io";
 (function() {
   var secret = "supersecretAPIkey";
   window.globalFunction(secret);
